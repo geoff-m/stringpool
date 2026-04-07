ef7d7c2ff3f00821e083cb716cb15f7a896720f0
Geoff McQueen <geoff@geoff.space>
2026-04-07 11:15:10 -0700

    Rework stats tests

--- test/src/TestStatistics.cpp
+++ test/src/TestStatistics.cpp
@@ -1,8 +1,40 @@
 #include <gtest/gtest.h>
 #include "stringpool/stringpool.h"
-#include "Utility.h"
 
 using namespace stringpool;
+
+// Not thread-safe.
+void expectInternHit(pool& p, const char* string, size_t length) {
+    const auto sizeBeforeHit = p.get_data_size();
+    const auto reqCountBeforeHit = p.get_total_intern_request_count();
+    const auto hitCountBeforeHit = p.get_total_intern_request_hits();
+    const auto missCountBeforeHit = p.get_total_intern_request_misses();
+    const auto reqSizeBeforeHit = p.get_total_intern_request_size();
+    EXPECT_LT(0, sizeBeforeHit);
+    p.intern(string, length); // hit.
+    const auto sizeAfterHit = p.get_data_size();
+    EXPECT_EQ(sizeBeforeHit, sizeAfterHit);
+    EXPECT_EQ(1, p.get_total_intern_request_count() - reqCountBeforeHit);
+    EXPECT_EQ(1, p.get_total_intern_request_hits() - hitCountBeforeHit);
+    EXPECT_EQ(missCountBeforeHit, p.get_total_intern_request_misses());
+    EXPECT_EQ(reqSizeBeforeHit + length, p.get_total_intern_request_size());
+}
+
+// Not thread-safe.
+void expectInternMiss(pool& p, const char* string, size_t length) {
+    const auto sizeBeforeMiss = p.get_data_size();
+    const auto reqCountBeforeMiss = p.get_total_intern_request_count();
+    const auto hitCountBeforeMiss = p.get_total_intern_request_hits();
+    const auto missCountBeforeMiss = p.get_total_intern_request_misses();
+    const auto reqSizeBeforeMiss = p.get_total_intern_request_size();
+    p.intern(string, length); // miss.
+    const auto sizeAfterMiss = p.get_data_size();
+    EXPECT_LT(sizeBeforeMiss, sizeAfterMiss);
+    EXPECT_EQ(1, p.get_total_intern_request_count() - reqCountBeforeMiss);
+    EXPECT_EQ(hitCountBeforeMiss, p.get_total_intern_request_hits());
+    EXPECT_EQ(1, p.get_total_intern_request_misses() - missCountBeforeMiss);
+    EXPECT_EQ(reqSizeBeforeMiss + length, p.get_total_intern_request_size());
+}
 
 TEST(Statistics, Empty) {
     pool p;
@@ -15,26 +47,17 @@ TEST(Statistics, Empty) {
 
 TEST(Statistics, OneMiss) {
     pool p;
-    const auto string = "foo";
-    p.intern(string);
-    EXPECT_LT(0, p.get_data_size());
-    EXPECT_EQ(1, p.get_total_intern_request_count());
-    EXPECT_EQ(0, p.get_total_intern_request_hits());
-    EXPECT_EQ(1, p.get_total_intern_request_misses());
-    EXPECT_EQ(strlen(string), p.get_total_intern_request_size());
+    expectInternMiss(p, "foo", 3);
 }
 
 TEST(Statistics, OneMissOneHit) {
     pool p;
-    const auto string = "foo";
-    p.intern(string); // miss
-    const auto sizeAfterMiss = p.get_data_size();
-    EXPECT_LT(0, sizeAfterMiss);
-    p.intern(string); // hit
-    const auto sizeAfterHit = p.get_data_size();
-    EXPECT_EQ(sizeAfterMiss, sizeAfterHit);
-    EXPECT_EQ(2, p.get_total_intern_request_count());
-    EXPECT_EQ(1, p.get_total_intern_request_hits());
-    EXPECT_EQ(1, p.get_total_intern_request_misses());
-    EXPECT_EQ(2 * strlen(string), p.get_total_intern_request_size());
+    expectInternMiss(p, "foo", 3);
+    expectInternHit(p, "foo", 3);
+}
+
+TEST(Statistics, OneMissEmpty) {
+    pool p;
+    expectInternMiss(p, "", 0);
+    expectInternHit(p, "", 0);
 }
